e4b08e82a8f7169897fe99a86d614db236fc97e6
Liyuanzhe2001 <619356147@qq.com>
2023-01-31 15:34:06 +0800

    update

--- src/main/java/com/lyz/ddedss_springboot/DdedssSpringbootApplication.java
+++ src/main/java/com/lyz/ddedss_springboot/DdedssSpringbootApplication.java
@@ -2,14 +2,24 @@ package com.lyz.ddedss_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class DdedssSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DdedssSpringbootApplication.class, args);
     }
 
+    /**
+     * 每日0:00检查是否有课程评价需要开始
+     */
+    @Scheduled(cron = "0 0 0 ? * *")
+    public void searchCourseEvaluation() {
+        System.out.println("定时任务");
+    }
 }
